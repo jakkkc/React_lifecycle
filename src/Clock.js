6623457e4319171e0120cb6a2fcf6c07a2ff2cb6
dexter <jacmwaniki@gmail.com>
2023-03-10 15:00:40 +0300

    First

---
+++ src/Clock.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+export class Clock extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+  componentDidMount() {
+    this.timerId = setInterval(() => {
+      this.setState({ date: new Date() });
+    }, 1000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.timerId);
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          <h1>{this.state.date.toLocaleString()}</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+// function Clock(props) {
+//   return <h1>{props.date.toLocaleString()}</h1>;
+// }
+export default Clock;
